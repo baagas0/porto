573869548bd339474d51988281a941f84b59f882
Ditya <baagas0@gmail.com>
2024-11-15 04:24:49 +0700

    First Init

---
+++ index.php
@@ -0,0 +1,262 @@
+<?php
+$items = [
+	[
+		'name'	=> 'Pembelajaran BPSDM',
+		'description' => "Frontend ReactJs - Pembelajaran Online",
+		'image'	=> 'LP/images/bpsdm.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'Jamdatun Video Conference',
+		'description' => "Frontend Vuejs - Video Conference",
+		'image'	=> 'LP/images/jamdatun.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'SIMRS - BUSEL',
+		'description' => "Fullstack - Vuejs & NodeJs",
+		'image'	=> 'LP/images/rsud-busel.png',
+		'url'	=> '#',
+	],
+
+
+	[
+		'name'	=> 'QHSE - SMKK',
+		'description' => "Quality, Healty, Safety, Enviroment - Add new module",
+		'image'	=> 'LP/images/qhse.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'Fuel Management',
+		'description' => "Fullstack - Microservice of RBS",
+		'image'	=> 'LP/images/fuel.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'SIMRS - Sistem Rumah Sakit',
+		'description' => "Backend NodeJs",
+		'image'	=> 'LP/images/asha.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'PLN JBT',
+		'image'	=> 'LP/images/jbt-image.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'Course',
+		'image'	=> 'LP/images/bimtek-preview.png',
+		'url'	=> 'https://bimtek.bbibnetwork.com/',
+	],
+	[
+		'name'	=> 'PPDB',
+		'image'	=> 'LP/images/ppdb-preview.png',
+		'url'	=> 'ppdb',
+	],
+	[
+		'name'	=> 'Ticketo',
+		'image'	=> 'LP/images/ticketo-preview.png',
+		'url'	=> 'https://ticketo.demo-web.my.id',
+	],
+	[
+		'name'	=> 'Marketplace API',
+		'image'	=> 'LP/images/api-preview.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'Company Profile',
+		'image'	=> 'LP/images/company-profile-preview.png',
+		'url'	=> 'company-profile',
+	],
+	[
+		'name'	=> 'Rataurant Management',
+		'image'	=> 'LP/images/restaurant.png',
+		'url'	=> 'https://resto.demo-web.my.id/login',
+	],
+	[
+		'name'	=> 'Buku Induk',
+		'image'	=> 'LP/images/buku-induk.png',
+		'url'	=> 'https://buku-induk.demo-web.my.id/master/login',
+	],
+	[
+		'name'	=> 'Tirai Mandalika',
+		'image'	=> 'LP/images/tiraimandalika.png',
+		'url'	=> 'https://tiraipelajarmandalika.or.id',
+	],
+	
+	[
+		'name'	=> 'Event Management',
+		'image'	=> 'LP/images/event-image.png',
+		'url'	=> '#',
+	],
+	[
+		'name'	=> 'Sistem Akademik',
+		'image'	=> 'LP/images/siadin2.png',
+		'url'	=> 'https://siadin.demo-web.my.id',
+	],
+	
+	[
+		'name'	=> 'Sistem Penjualan Lisensi API Whatsapp',
+		'image'	=> 'LP/images/wa-center.png',
+		'url'	=> '#',
+	],
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<title>Bagas Aditya Mahendra</title>
+	<meta name="title" content="Ditya Dev">
+	<meta name="description" content="Saya bersemangat membangun website developer, software, dan tools luar biasa yang meningkatkan dan berguna bagi kehidupan orang-orang di sekitar saya dan klien saya. Saya mengkhususkan diri dalam membuat website developer untuk klien mulai dari individu dan bisnis kecil hingga company perusahaan. ">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://bagasawan.me/">
+	<meta property="og:title" content="Bagas Aditya Mahendra">
+	<meta property="og:description" content="Saya bersemangat membangun website developer, software, dan tools luar biasa yang meningkatkan dan berguna bagi kehidupan orang-orang di sekitar saya dan klien saya. Saya mengkhususkan diri dalam membuat website developer untuk klien mulai dari individu dan bisnis kecil hingga company perusahaan. ">
+	<meta property="og:image" content="">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="http://bagasawan.me/">
+	<meta property="twitter:title" content="Bagas Aditya Mahendra">
+	<meta property="twitter:description" content="Saya bersemangat membangun website developer, software, dan tools luar biasa yang meningkatkan dan berguna bagi kehidupan orang-orang di sekitar saya dan klien saya. Saya mengkhususkan diri dalam membuat website developer untuk klien mulai dari individu dan bisnis kecil hingga company perusahaan. ">
+	<meta property="twitter:image" content="">
+	<link rel="favicon icon" href="LP/images/favicon.png">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="LP/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="LP/fonts/flat-icon/flaticon.css">
+	<link rel="stylesheet" href="LP/styles/bootstrap.css" />
+	<link rel="stylesheet" href="LP/styles/main.css" />
+</head>
+
+<body class="demo-page">
+
+	<!-- Preloader -->
+	<div id="js-preloader" class="js-preloader">
+		<div class="content">
+			<img src="LP/images/logo-icon.png" alt="">
+		</div>
+		<div class="preloader-inner"></div>
+	</div>
+
+
+	<!-- Main Banner -->
+	<section class="demo-page-baner">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="demo-logo">
+						<img src="LP/images/BG-TR4.png" alt="">
+					</div>
+					<h2>Setup a <em>stunning</em> website with <em>Ditya</em>.</h2>
+					<div class="scroll-to-section">
+						<div class="main-white-button">
+							<a href="#demos">Check Demos Now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div class="intro">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<h2>Website build by <em>NodeJs, Laravel, VueJs</em> Framework</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="demos"></div>
+	<div class="demo">
+		<div class="container-fluid">
+			<div class="row">
+				<?php
+				foreach ($items as $key => $item) {
+				?>
+					<div class="col-lg-4">
+						<div class="demo-item">
+							<div class="demo-thumb">
+								<a href="<?php echo $item['url'] ?>">
+									<img src="<?php echo $item['image'] ?>" alt="" style="border: 1px solid #919191">
+								</a>
+							</div>
+							<div class="down-content" style="padding: 20px">
+								<h4 style="padding: 0px"><a href="<?php echo $item['url'] ?>"><?php echo $item['name'] ?></a></h4>
+								<small><?= $item['description'] ?? '100% Done' ?></small>
+							</div>
+						</div>
+					</div>
+				<?php
+				}
+				?>
+				<!-- ======================================================================================= -->
+				<div class="col-lg-4">
+					<div class="demo-item coming-soon">
+						<div class="demo-thumb">
+							<h4>&amp;<br>Much More Coming Soon<br>...</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Footer -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="footer-cta">
+						<div class="row">
+							<div class="col-lg-7 text-left">
+								<div class="section-heading">
+									<h1>Bagas Aditya Mahendra</h1>
+									<h2>Bagas Aditya Mahendra</h2>
+									<p>Website developer build by NodeJs, Laravel, VueJs Framework</p>
+								</div>
+							</div>
+							<div class="col-lg-5 text-right align-self-center">
+								<div class="main-white-button">
+									<a href="https://api.whatsapp.com/send/?phone=6289506373551&text&app_absent=0">Contact Me!</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
+	<!-- Scripts -->
+	<script src="LP/scripts/vendors/jquery-3.4.1.min.js"></script>
+	<script src="LP/scripts/vendors/jquery.hoverIntent.min.js"></script>
+	<script src="LP/scripts/vendors/perfect-scrollbar.min.js"></script>
+	<script src="LP/scripts/vendors/jquery.easing.min.js"></script>
+	<script src="LP/scripts/vendors/wow.min.js"></script>
+	<script src="LP/scripts/vendors/parallax.min.js"></script>
+	<script src="LP/scripts/vendors/isotope.min.js"></script>
+	<script src="LP/scripts/vendors/imagesloaded.pkgd.min.js"></script>
+	<script src="LP/scripts/vendors/packery-mode.pkgd.min.js"></script>
+	<script src="LP/scripts/vendors/owl-carousel.min.js"></script>
+	<script src="LP/scripts/vendors/jquery.appear.js"></script>
+	<script src="LP/scripts/vendors/jquery.countTo.js"></script>
+	<script src="LP/scripts/vendors/slide-nav.min.js"></script>
+	<script src="LP/scripts/main.js"></script>
+
+
+</body>
+
+</html>
